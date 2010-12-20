e931f5d66130b143b5802ccfc2109f36430c850b
Travelgrouper <matthew.guidi@travelgrouper.com>
2010-12-19 20:17:36 -0700

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myfirstapp::Application.config.secret_token = '82eb9c7c82a43ef9f50b7a10204e18c5e47722ce2571291b899b296d376b93b03fc5fd126c45fdbb7dfe7cef3dceb663fb1b07649113580508b1024ef1b7a2cc'
